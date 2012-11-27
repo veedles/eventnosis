d55fb0f1d84b179ec457e417cef74c75bf100ba5
Declan McGrath <declanmg@gmail.com>
2012-11-27 20:57:37 +0000

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventsys::Application.config.secret_token = '327d755b7bff4e0cc0a3208f806849952d6936143845fb4721f16638b98c4972089819cdfe79122bd1ebe2e50830f882d6c7e4c3ff8fcba35ee591167cc136a0'
